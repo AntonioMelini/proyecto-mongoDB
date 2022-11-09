171d4ada995b98918ead6dc5f9eef2bc9b89c4ca
amelini <antoniomelini@hotmail.com>
2022-11-09 15:18:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//mongodb+srv://antonio:Casaanto1@cluster0.jeyfg0g.mongodb.net/dbapi?retryWrites=true&w=majority
+require("dotenv").config();
+const expres=require("express");
+const cors=require("cors");
+
+const app = expres();
+
+app.use(cors());
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("aplicacion escuchando en el puerto `http://localhost:3000/`");
+})
